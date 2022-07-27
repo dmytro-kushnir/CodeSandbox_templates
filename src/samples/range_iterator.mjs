3a43d3f9900a824774481aa8c6aeee5727330bc2
Dmytro Kushnir <dimakush1@gmail.com>
2022-07-27 11:26:42 +0300

    Add rangle class iterator

--- src/samples/range_iterator.mjs
+++ src/samples/range_iterator.mjs
@@ -16,8 +16,30 @@ export default function range_iterator(start, end, step = 1) {
   };
 }
 
+
+class Collection {
+  // or we can create simple generator function: *getIterator() {...} instead
+  // but then below: let value of myCollection.getIterator()
+
+  *[Symbol.iterator]() {
+    let i = 0;
+    while(this[i] !== undefined) {
+      yield this[i];
+      ++i;
+    }
+  }
+}
+
+
 (() => {
   for (const n of range_iterator(0, 100, 5)) {
     console.log('!!! range', n);
+  }
+
+  const myCollection = new Collection();
+  myCollection[0] = 1;
+  myCollection[1] = 3;
+  for(let value of myCollection) {
+      console.log(value); // 1, then 2
   }
 })()
